354e179851e33eda6069960fb37ddeb1d0e560a2
Sam Bloomberg <sam@redxdev.com>
2016-10-20 09:44:19 -0400

    Some notes about ECS

---
+++ include/ECS.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Not currently ready to be integrated with our stuff, still a bit to do.
+// See https://github.com/redxdev/ECS for current progress on the ECS since
+// it's being developed as a single-header library that isn't specific to
+// our project. It'll be copied over here eventually
