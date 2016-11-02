dce4c7ebb2bdd16e2d0f31d6eef85e3edee34967
Samuel Sternklar (RIT Student) <sis2773@ad.rit.edu>
2016-11-02 16:34:30 -0400

    Spline mesh generation beginnings

--- include/Spline.h
+++ include/Spline.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "SplinePiece.h"
+#include "Vertex.h"
 
 class Spline
 {
 public:
-	int segmentCount;
+	unsigned int segmentCount;
 	SplinePiece* segments;
 	void GetPoint(float t, SplineControlPoint& outPoint);
 	float GuessNearestPoint(DirectX::XMFLOAT3& point, float* outDistanceSquared = nullptr);
 	void GuessNearestPoint(DirectX::XMFLOAT3& point, SplineControlPoint& outPoint);
+	void GenerateMesh(Vertex* vertex, unsigned int numVerts);
 };
