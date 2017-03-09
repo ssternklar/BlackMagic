a78bfdd120ff5c6fb421ea1c312ca9763f50302b
Sam Willis <sw9662@g.rit.edu>
2017-03-09 12:18:48 -0800

    Added Use and Set methods to Material

--- include/Material.h
+++ include/Material.h
@@ -10,7 +10,7 @@
 
 using BlackMagic::Texture;
 
-enum AttributeStage : int
+enum ResourceStage : int
 {
 	VertexShader,	
 	HullShader,
@@ -20,32 +20,46 @@ enum AttributeStage : int
 	ComputeShader
 };
 
-template<typename T>
-static std::shared_ptr<T> _optional_ptr = std::shared_ptr<T>(nullptr);
-
 class Material
 {
 public:
 	Material(
 		const std::shared_ptr<SimpleVertexShader>& vs, 
 		const std::shared_ptr<SimplePixelShader>& ps,
-		const std::shared_ptr<SimpleHullShader>& hs = _optional_ptr<SimpleHullShader>,
-        const std::shared_ptr<SimpleDomainShader>& ds = _optional_ptr<SimpleDomainShader>
+		const std::shared_ptr<SimpleHullShader>* hs = nullptr,
+        const std::shared_ptr<SimpleDomainShader>* ds = nullptr,
+        const std::shared_ptr<SimpleGeometryShader>* gs = nullptr,
+        const std::shared_ptr<SimpleComputeShader>* cs = nullptr
 	);
 
 	SimpleVertexShader* VertexShader() const;
 	SimplePixelShader* PixelShader() const;
-	
+    SimpleHullShader* HullShader() const;
+    SimpleDomainShader* DomainShader() const;
+    SimpleGeometryShader* GeometryShader() const;
+    SimpleComputeShader* ComputeShader() const;
 
-	void Apply(
-		DirectX::XMFLOAT4X4 view,
-		DirectX::XMFLOAT4X4 proj);
-	void Upload();
+    //Turns on shader stages and uploads data
+    void Use() const;
 
-	bool operator ==(const Material&) const;
+    template<typename T>
+    void Set(ResourceStage s, T& data);
+
 private:
+    bool _active = false;
 	std::shared_ptr<SimpleVertexShader> _vertShader;
 	std::shared_ptr<SimplePixelShader> _pixelShader;
 	std::shared_ptr<Texture> _albedo, _roughness, _metalness, _cavity, _normal;
 	ComPtr<ID3D11SamplerState> _sampler;
 };
+
+#if defined(_WIN32) || defined(_WIN64)
+template void Material::Set<int>(ResourceStage s, int& data);
+template void Material::Set<float>(ResourceStage s, float& data);
+template void Material::Set<DirectX::XMFLOAT2>(ResourceStage s, DirectX::XMFLOAT2& data);
+template void Material::Set<DirectX::XMFLOAT3>(ResourceStage s, DirectX::XMFLOAT3& data);
+template void Material::Set<DirectX::XMFLOAT4>(ResourceStage s, DirectX::XMFLOAT4& data);
+template void Material::Set<DirectX::XMFLOAT4X4>(ResourceStage s, DirectX::XMFLOAT4X4& data);
+template void Material::Set<ID3D11ShaderResourceView*>(ResourceStage s, ID3D11ShaderResourceView*& data);
+template void Material::Set<ID3D11SamplerState*>(ResourceStage s, ID3D11SamplerState*& data);
+#endif
